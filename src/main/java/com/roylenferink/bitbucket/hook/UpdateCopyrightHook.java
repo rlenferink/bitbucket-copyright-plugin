d0a3293a0d7a906eaa05051f1c5bca78e734f078
Roy Lenferink <rlenferink@apache.org>
2019-06-06 22:52:31 +0200

    Only logging on merge of pull request

--- src/main/java/com/roylenferink/bitbucket/hook/UpdateCopyrightHook.java
+++ src/main/java/com/roylenferink/bitbucket/hook/UpdateCopyrightHook.java
@@ -2,19 +2,19 @@ package com.roylenferink.bitbucket.hook;
 
 import com.atlassian.bitbucket.hook.repository.PostRepositoryHook;
 import com.atlassian.bitbucket.hook.repository.PostRepositoryHookContext;
-import com.atlassian.bitbucket.hook.repository.RepositoryHookRequest;
+import com.atlassian.bitbucket.hook.repository.PullRequestMergeHookRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 import java.util.stream.Collectors;
 
-public class UpdateCopyrightHook implements PostRepositoryHook<RepositoryHookRequest> {
+public class UpdateCopyrightHook implements PostRepositoryHook<PullRequestMergeHookRequest> {
 
     private static final Logger log = LoggerFactory.getLogger(UpdateCopyrightHook.class);
 
     @Override
-    public void postUpdate(@Nonnull PostRepositoryHookContext context, @Nonnull RepositoryHookRequest hookRequest) {
+    public void postUpdate(@Nonnull PostRepositoryHookContext context, @Nonnull PullRequestMergeHookRequest hookRequest) {
         log.info("[{}] {} updated [{}]",
                 hookRequest.getRepository(),
                 hookRequest.getTrigger().getId(),
