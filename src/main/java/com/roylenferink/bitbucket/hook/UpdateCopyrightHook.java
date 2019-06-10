3399de967293f00b616b7ed48e97f1e94fc8fb92
Roy Lenferink <lenferinkroy@gmail.com>
2019-06-10 17:54:35 +0200

    A list with changed files is collected now

--- src/main/java/com/roylenferink/bitbucket/hook/UpdateCopyrightHook.java
+++ src/main/java/com/roylenferink/bitbucket/hook/UpdateCopyrightHook.java
@@ -1,31 +1,67 @@
 package com.roylenferink.bitbucket.hook;
 
+import com.atlassian.bitbucket.commit.Changeset;
+import com.atlassian.bitbucket.commit.ChangesetsRequest;
+import com.atlassian.bitbucket.commit.CommitService;
+import com.atlassian.bitbucket.content.Change;
+import com.atlassian.bitbucket.content.Path;
 import com.atlassian.bitbucket.hook.repository.PostRepositoryHook;
 import com.atlassian.bitbucket.hook.repository.PostRepositoryHookContext;
 import com.atlassian.bitbucket.hook.repository.PullRequestMergeHookRequest;
+import com.atlassian.bitbucket.util.Page;
+import com.atlassian.bitbucket.util.PageRequestImpl;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.roylenferink.bitbucket.logger.PluginLoggerFactory;
 import org.slf4j.Logger;
 
 import javax.annotation.Nonnull;
-import java.util.stream.Collectors;
+import java.util.HashSet;
+import java.util.Set;
 
 public class UpdateCopyrightHook implements PostRepositoryHook<PullRequestMergeHookRequest> {
 
-    private PluginLoggerFactory lf = PluginLoggerFactory.getInstance();
-    private final Logger log = lf.getLoggerForThis(this);
+    private final Logger log;
+    private final CommitService commitService;
+
+    public UpdateCopyrightHook(@ComponentImport final CommitService commitService) {
+        PluginLoggerFactory lf = PluginLoggerFactory.getInstance();
+        this.log =  lf.getLoggerForThis(this);
+        this.commitService = commitService;
+    }
 
     @Override
     public void postUpdate(@Nonnull PostRepositoryHookContext context, @Nonnull PullRequestMergeHookRequest request) {
         if (request.isDryRun())
-            return; //Don't do anything if a dryrun is being executed
+            return; // Don't do anything if a dryrun is being executed
 
-        log.info("YEAHOOOOOO!!!!");
-        log.info("[{}] {} updated [{}]",
+        if (!request.getMergeHash().isPresent())
+            return; // No merge commit available
+
+        String mergeCommit = request.getMergeHash().get();
+
+        log.debug("Pull request in [{}] merged from [{}] to [{}] in commit [{}]",
                 request.getRepository(),
-                request.getTrigger().getId(),
-                request.getRefChanges().stream()
-                    .map(change -> change.getRef().getId())
-                    .collect(Collectors.joining(", ")));
+                request.getFromRef().getId(),
+                request.getToRef().getId(),
+                mergeCommit);
+
+        ChangesetsRequest csr = new ChangesetsRequest.Builder(request.getRepository())
+                                    .commitId(mergeCommit)
+                                    .build();
+
+        Set<Path> changedFiles = new HashSet<>();
+
+        Page<Changeset> changesets = commitService.getChangesets(csr, new PageRequestImpl(0, 9999));
+        for (Changeset cs : changesets.getValues()) {
+            Page<Change> changes = cs.getChanges();
+            for (Change c : changes.getValues()) {
+                changedFiles.add(c.getPath());
+            }
+        }
+
+        for (Path p : changedFiles) {
+            log.debug("File changed in PR: {}", p.toString());
+        }
     }
 
 }
