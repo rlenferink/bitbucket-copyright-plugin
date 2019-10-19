2a7002382c4626c01cb5259f540f63714e75d2ca
Roy Lenferink <lenferinkroy@gmail.com>
2019-10-19 14:49:38 +0200

    Reading and printing file contents

--- src/main/java/com/roylenferink/bitbucket/hook/UpdateCopyrightHook.java
+++ src/main/java/com/roylenferink/bitbucket/hook/UpdateCopyrightHook.java
@@ -3,11 +3,11 @@ package com.roylenferink.bitbucket.hook;
 import com.atlassian.bitbucket.commit.Changeset;
 import com.atlassian.bitbucket.commit.ChangesetsRequest;
 import com.atlassian.bitbucket.commit.CommitService;
-import com.atlassian.bitbucket.content.Change;
-import com.atlassian.bitbucket.content.Path;
+import com.atlassian.bitbucket.content.*;
 import com.atlassian.bitbucket.hook.repository.PostRepositoryHook;
 import com.atlassian.bitbucket.hook.repository.PostRepositoryHookContext;
 import com.atlassian.bitbucket.hook.repository.PullRequestMergeHookRequest;
+import com.atlassian.bitbucket.io.TypeAwareOutputSupplier;
 import com.atlassian.bitbucket.util.Page;
 import com.atlassian.bitbucket.util.PageRequestImpl;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
@@ -15,6 +15,10 @@ import com.roylenferink.bitbucket.logger.PluginLoggerFactory;
 import org.slf4j.Logger;
 
 import javax.annotation.Nonnull;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -22,11 +26,15 @@ public class UpdateCopyrightHook implements PostRepositoryHook<PullRequestMergeH
 
     private final Logger log;
     private final CommitService commitService;
+    private final ContentService contentService;
 
-    public UpdateCopyrightHook(@ComponentImport final CommitService commitService) {
+    public UpdateCopyrightHook(@ComponentImport final CommitService commitService,
+                               @ComponentImport final ContentService contentService) {
         PluginLoggerFactory lf = PluginLoggerFactory.getInstance();
         this.log =  lf.getLoggerForThis(this);
+
         this.commitService = commitService;
+        this.contentService = contentService;
     }
 
     @Override
@@ -51,8 +59,8 @@ public class UpdateCopyrightHook implements PostRepositoryHook<PullRequestMergeH
 
         Set<Path> changedFiles = new HashSet<>();
 
-        Page<Changeset> changesets = commitService.getChangesets(csr, new PageRequestImpl(0, 9999));
-        for (Changeset cs : changesets.getValues()) {
+        Page<Changeset> changeSets = commitService.getChangesets(csr, new PageRequestImpl(0, 9999));
+        for (Changeset cs : changeSets.getValues()) {
             Page<Change> changes = cs.getChanges();
             for (Change c : changes.getValues()) {
                 changedFiles.add(c.getPath());
@@ -61,6 +69,24 @@ public class UpdateCopyrightHook implements PostRepositoryHook<PullRequestMergeH
 
         for (Path p : changedFiles) {
             log.debug("File changed in PR: {}", p.toString());
+
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            contentService.streamFile(request.getRepository(), mergeCommit, p.toString(), new TypeAwareOutputSupplier() {
+                @Override
+                @Nonnull
+                public OutputStream getStream(@Nonnull String s) throws IOException {
+                    return os;
+                }
+            });
+
+            try {
+                String fileContent = new String(os.toByteArray(), "UTF-8");
+                log.debug("==================");
+                log.debug(fileContent);
+                log.debug("==================");
+            } catch (UnsupportedEncodingException e) {
+                log.error("Error parsing file content: {}", e.getMessage());
+            }
         }
     }
 
